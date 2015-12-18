c1383cf8414d2f3b60c62918f869d39acbcff885
Jong Kyu, Lim <jklim1253@gmail.com>
2015-12-18 19:10:41 +0900

    append Day 01 - part two.

--- Day-01/day01.cpp
+++ Day-01/day01.cpp
@@ -6,10 +6,12 @@ using namespace std;
 class Santa {
 private :
 	int _floor;
+	int _position;
+	bool _trigger;
 	static const char up = '(';
 	static const char down = ')';
 public :
-	Santa() : _floor(0){}
+	Santa() : _floor(0), _position(0), _trigger(false) {}
 
 	inline void go_down() {
 		--_floor;
@@ -26,10 +28,30 @@ public :
 			if (direction == up) go_up();
 			else if (direction == down) go_down();
 			else unknown_instruction(direction);
+
+			// trigger check
+			trigger();
 		}
 	}
 	void unknown_instruction(const char& what) const {
 		cout << "unknown instruction : " << what << endl;
+	}
+	bool trigger() {
+		// if trigger was already invoked
+		if (_trigger)
+			return _trigger;
+
+		// for check when trigger is invoked.
+		++_position;
+
+		// condition of which trigger occur
+		if (_floor < 0)
+			_trigger = true;
+
+		return _trigger;
+	}
+	int when_trigger_invoked() const {
+		return _position;
 	}
 };
 
@@ -51,6 +73,7 @@ int main(int argc, char* argv[]) {
 	goodman.follow_instruction(file);
 
 	cout << "Santa\'s floor is " << goodman.where_am_i() << endl;
+	cout << "first time Santa entering basement : " << goodman.when_trigger_invoked() << endl;
 
 	file.close();
 
