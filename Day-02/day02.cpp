f09b9d0f896f3f7dc8977c61a37007761a836abe
Jong Kyu, Lim <jklim1253@gmail.com>
2015-12-18 20:31:08 +0900

    add Day 02 part two.

--- Day-02/day02.cpp
+++ Day-02/day02.cpp
@@ -19,13 +19,17 @@ public :
 	inline int max() const {
 		return std::max(length, std::max(width, height));
 	}
+	inline int ribbon() const {
+		return length*width*height + 2 * (length + width + height - max());
+	}
 };
 
 class Elf {
 private :
 	int papers;
+	int ribbons;
 public :
-	Elf() : papers(0) {}
+	Elf() : papers(0), ribbons(0) {}
 	void follow_instruction(istream& is) {
 		int length, width, height;
 		char x;
@@ -33,10 +37,14 @@ public :
 		while (is >> length >> x >> width >> x >> height) {
 			Box present(length, width, height);
 			papers += present.surface();
+			ribbons += present.ribbon();
 		}
 	}
 	inline int total_papaer() const {
 		return papers;
+	}
+	inline int total_ribbon() const {
+		return ribbons;
 	}
 };
 int main(int argc, char* argv[]) {
@@ -56,6 +64,7 @@ int main(int argc, char* argv[]) {
 	elf.follow_instruction(file);
 
 	cout << "Elf needs paper of which size is " << elf.total_papaer() << endl;
+	cout << "Elf needs ribbon of which size is " << elf.total_ribbon() << endl;
 
 	file.close();
 
